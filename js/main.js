abec45fac1fa876790cc970b9311447001a022a2
sango224 <sango.xx0224@gmail.com>
2024-03-16 17:07:17 +0900

    3/16

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var numA = 20;
+var numB = 40;
+if (numA == 20 && numB == 40) {
+  console.log("ANDの条件と一致した処理");
+} else {
+  console.log("ANDの条件と一致しなかった処理");
+}
+
+var numA = 20;
+var numB = 40;
+if (numA == 20 || numB == 40) {
+  console.log("ORの条件と一致した処理");
+} else {
+  console.log("ORの条件と一致しなかった処理");
+}
+
+var num = 20;
+if (num != 10) {
+  console.log("NOTの条件と一致した処理");
+} else {
+  console.log("NOTの条件と一致しなかった処理");
+}
